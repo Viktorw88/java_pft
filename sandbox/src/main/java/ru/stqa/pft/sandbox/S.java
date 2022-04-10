a38bd11bd7a256dbdba413848b82ca2beff84776
Viktor Ivanitskii <94568542+Viktorw88@users.noreply.github.com>
2022-04-10 18:04:24 +0300

    Create S.java

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/S.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+public class S {
+    public double l;
+
+    public S(double l) {
+      this.l = l;
+    }
+
+    public double area() {
+      return this.l * this.l;
+    }
+
+  }
+
+
