1ca341472cf37fc018a88fe4ab0c8a8459f30821
wizz <wizzjke88@gmail.com>
2022-04-10 17:58:44 +0300

    class s

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/S.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+public class S {
+    public double l;
+
+    public S(double l) {
+      this.l = l;
+    }
+
+    public double area() {
+      return this.l * this.l;
+    }
+
+  }
+
+
+
